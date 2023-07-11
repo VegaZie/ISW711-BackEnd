00ef0e5ba5d9e5b438b6dcfac5c525134a3c1a61
VegaZie <davidgerardovegas29@gmail.com>
2023-07-11 09:12:51 -0600

    Se agrego el userID al promtModel

--- models/promtModel.js
+++ models/promtModel.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const promtSchema = new mongoose.Schema({
     model: { type: String },
     input: { type: String },
-    instruction: {type: String}
+    instruction: {type: String},
+    userID: { type: String}
   });
   
   module.exports = mongoose.model('Promt', promtSchema);
