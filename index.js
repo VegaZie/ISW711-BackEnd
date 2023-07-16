8203e20d766b00e58559592c705ee365f67fd002
VegaZie <davidgerardovegas29@gmail.com>
2023-07-16 16:45:27 -0600

    implemetación de verificación del jwt token como middleware

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
+
 // database connection
 const mongoose = require("mongoose");
 const db = mongoose.connect(process.env.MONGO_DATABASE_URL);
+const secretKey = process.env.SECRET_KEY;
+const User = require("../models/userModel");
 
+const jwt = require("jsonwebtoken");
 const {
   userPost,
   userGet,
@@ -12,10 +16,7 @@ const {
   userDelete,
 } = require("./controllers/userController.js");
 
-const {
-  authenticate,
-  verifyToken,
-} = require("./controllers/sessionController");
+const { authenticate } = require("./controllers/sessionController");
 
 const {
   promtDelete,
@@ -37,23 +38,51 @@ app.use(
   })
 );
 
+// Session
+app.post("/api/authenticate", authenticate);
+
+//user create
+app.post("/api/user", userPost);
+
+// Middleware para verificar el token JWT en las rutas protegidas
+app.use(async function (req, res, next) {
+  const token = req.headers["authorization"];
+
+  if (!token) {
+    return res.status(401).json({ error: "Token no proporcionado" });
+  }
+
+  try {
+    // Verificar y decodificar el token
+    const decoded = jwt.verify(token, secretKey);
+
+    // Verificar la existencia del usuario en la base de datos
+    const user = await User.findOne({ email: decoded.email });
+    if (!user) {
+      return res.status(401).json({ error: "Usuario no encontrado" });
+    }
+
+    // Almacenar el usuario en el objeto de solicitud para usarlo en otras rutas
+    req.user = user;
+    next(); // Continuar con la siguiente función en la cadena de middleware
+  } catch (error) {
+    if (error instanceof jwt.TokenExpiredError) {
+      return res.status(401).json({ error: "Token expirado" });
+    }
+    console.error("Error al verificar el token:", error);
+    res.status(401).json({ error: "Token inválido" });
+  }
+});
+
 // User
 app.get("/api/user", userGet);
-app.post("/api/user", userPost);
 app.patch("/api/user", userPatch);
 app.delete("/api/user", userDelete);
 
-// Session
-app.post("/api/authenticate", authenticate);
-app.post("/api/verify", verifyToken);
-
 // Promt
-
 app.post("/api/promt", promtPost);
 app.get("/api/promt", promtGet);
 app.patch("/api/promt", promtPatch);
 app.delete("/api/promt", promtDelete);
-
-
 
 app.listen(3001, () => console.log(`Listening on port 3001!`));
