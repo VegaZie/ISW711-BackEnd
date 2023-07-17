503f291cc05967b3385507aebfa252b657918011
VegaZie <davidgerardovegas29@gmail.com>
2023-07-17 12:07:03 -0600

    Se creó un promt model unico para mejor compresión de código y se eliminaron los 3 modelos creados anteriormente

---
+++ models/promtModel.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const promtSchema = new mongoose.Schema({
+  model: { type: String },
+  input: { type: String },
+  instruction: { type: String },
+  prompt: { type: String },
+  temperature: { type: Number },
+  quantity: { type: String },
+  size: { type: String },
+  response: { type: String },
+  userID: { type: String },
+});
+
+module.exports = mongoose.model('Promt', promtSchema);
