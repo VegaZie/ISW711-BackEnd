8548b426b38a98bfd5850c7272b2b200977edd5c
VegaZie <davidgerardovegas29@gmail.com>
2023-07-17 20:43:23 -0600

    Se elimino el try catch de los metodos de edit y completions ya que generaban un error interno que no existía

--- controllers/openAiController.js
+++ controllers/openAiController.js
@@ -12,18 +12,21 @@ const configuration = new Configuration({
  * @param {*} res - Objeto de respuesta de Express.
  */
 const executeCompletionsPrompt = async (req, res) => {
-  try {
-    const openai = new OpenAIApi(configuration);
-    const response = await openai.createCompletion({
-      model: req.body.model,
-      prompt: req.body.prompt,
-      temperature: req.body.temperature,
-    });
+  const openai = new OpenAIApi(configuration);
+  const response = await openai.createCompletion({
+    model: req.body.model,
+    prompt: req.body.prompt,
+    temperature: req.body.temperature,
+  });
 
-    res.status(200).json(response); // Responde con el resultado de la solicitud exitosa al cliente
-  } catch (error) {
-    console.error("Error en la solicitud a OpenAI:", error);
-    res.status(500).json({ error: "Error en la solicitud a OpenAI" }); // Responde con un error al cliente en caso de falla
+  if (response) {
+    res.status(201); // CREATED
+    res.json(response.data);
+  } else {
+    res.status(422);
+    res.json({
+      message: "Hubo un error al ejecutar el método de Open AI",
+    });
   }
 };
 
@@ -34,18 +37,21 @@ const executeCompletionsPrompt = async (req, res) => {
  * @param {*} res - Objeto de respuesta de Express.
  */
 const executeEditPromt = async (req, res) => {
-  try {
-    const openai = new OpenAIApi(configuration);
-    const response = await openai.createEdit({
-      model: req.body.model,
-      input: req.body.input,
-      instruction: req.body.instruction,
-    });
+  const openai = new OpenAIApi(configuration);
+  const response = await openai.createEdit({
+    model: req.body.model,
+    input: req.body.input,
+    instruction: req.body.instruction,
+  });
 
-    res.status(200).json(response); // Responde con el resultado de la solicitud exitosa al cliente
-  } catch (error) {
-    console.error("Error en la solicitud a OpenAI:", error);
-    res.status(500).json({ error: "Error en la solicitud a OpenAI" }); // Responde con un error al cliente en caso de falla
+  if (response) {
+    res.status(201); // CREATED
+    res.json(response.data);
+  } else {
+    res.status(422);
+    res.json({
+      message: "Hubo un error al ejecutar el método de Open AI",
+    });
   }
 };
 
