f2443514d587c8e776634ef96b0ad6ada6a53c6f
VegaZie <davidgerardovegas29@gmail.com>
2023-07-26 18:16:58 -0600

    Se actualizó el modelo de promt

--- models/promtModel.js
+++ models/promtModel.js
@@ -1,16 +1,18 @@
 const mongoose = require("mongoose");
 
 const promtSchema = new mongoose.Schema({
+  name: { type: String },
   model: { type: String },
   input: { type: String },
   instruction: { type: String },
-  prompt: { type: String },
+  promt: { type: String },
   temperature: { type: Number },
   quantity: { type: String },
   size: { type: String },
   response: { type: String },
   userID: { type: String },
   tags: { type: [String] },
+  type: { type: String },
 });
 
 module.exports = mongoose.model("Promt", promtSchema);
